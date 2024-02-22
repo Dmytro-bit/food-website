eaa64b67081235301b89bf27b9cc2641faf786c8
Dmytro <dimabokov5@gmail.com>
2024-02-22 10:16:05 +0000

    Animations and some hover effects added

--- js/script.js
+++ js/script.js
@@ -4,16 +4,37 @@ window.onload = () => {
     fetch(url)
     .then(response => response.json())
     .then(jsonData => {
-        let htmlString = `<table>`;
 
-        jsonData.forEach(food => {
-            htmlString += `<tr>`;
-            htmlString += `<td>${food.id}</td>`;
-            htmlString += `<td>${food.name}</td>`;
-            htmlString += `</tr>`;
+        let foods = jsonData
+
+        let htmlString = `<table><tr>`;
+        let counter = 0
+
+        foods.forEach(food => {
+
+            if(counter < 3)
+            {
+                htmlString += `<td><div class="element"><div class="element_photo"></div>
+                                <div class="element_info"><h1 style="padding: 
+                                ${
+                                    food.name.length <= 10 ? `0vw 0vw 2.5vw 0vw; ` : `0vw 0vw 1.5vw 0vw; `
+                                }
+                                font-size: 
+                                ${
+                                    food.name.length >= 17 ? `1.3vw` : `1.5vw`
+                                }
+                                ">${food.name}</h1>
+                                </div></div></td>`;
+                counter++
+            }
+            else
+            {
+                htmlString += `<tr>`
+                counter = 0
+            }
         });
 
-        htmlString += `</table>`;
+        htmlString += `</tr></table>`;
 
         document.getElementById("main_data").innerHTML = htmlString; // Fix: Set innerHTML property
     });
