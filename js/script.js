5093fe6cd46f5f13434d121324018333e0aa4769
Dmytro <dimabokov5@gmail.com>
2024-02-26 16:44:02 +0000

    mobile version compliete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let foods = []
 let sort_field = "name"
 let sort = 1
 let roll = 4 // how many products in the roll
+let font_size = 1.3
+let padding = 2.5
 const window_width = window.screen.availWidth
 
 window.onload = () => {
@@ -21,13 +23,13 @@ function displayTable() {
 
     let htmlString = `<table><tr>`;
     let counter = 0
-
+    elementDisplay()
     foods.forEach(food => {
 
-        if (counter < elementDisplay(roll)) {
+        if (counter < roll) {
             htmlString += `<td><div class="element"><div class="element_photo"></div>
-                                <div class="element_info"><h1 style="padding: ${food.name.length <= 10 ? `0vw 0vw 2.5vw 0vw; ` : `0vw 0vw 1.5vw 0vw; `} font-size: 
-                                ${food.name.length >= 17 ? `1.3vw` : `1.5vw`}">${food.name}</h1></div></div></td>`;
+                                <div class="element_info"><h1 style="padding: ${food.name.length <= 10 ? `0vw 0vw ${padding}vw 0vw; ` : `0vw 0vw ${padding*0.85}vw 0vw; `} font-size: 
+                                ${food.name.length >= 17 ? `${font_size}vw` : ''}">${food.name}</h1></div></div></td>`;
             counter++
         } else {
             htmlString += `<tr>`
@@ -46,9 +48,9 @@ function Sort(field) {
     displayTable();
 }
 
-function elementDisplay(roll) {
+function elementDisplay() {
     if (window_width <= 480) {
-        return roll / 2
+        roll = 1
+        font_size = 5
     }
-    return roll
 }
